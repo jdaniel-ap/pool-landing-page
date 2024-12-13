f5421c629ce08323e5b4852f46f43e277c69f580
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2024-12-12 22:40:30 -0300

    fix: cloudfare functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare({
+    imageService: "cloudflare",
+  }),
   integrations: [tailwind(), react()],
 });
