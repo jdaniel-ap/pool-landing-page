b80f316ae4afea842850b2cc395fbf9d274150b8
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2024-12-13 14:34:07 -0300

    feat: add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://grupo-bennu.com/",
   output: "server",
   adapter: cloudflare({
     imageService: "cloudflare",
   }),
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
